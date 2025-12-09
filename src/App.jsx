1b8dd1840a6baaadab0c1e0cbc75959fbbc09e4b
LuxS94 <simonetti.lucio@libero.it>
2025-12-09 16:50:48 +0100

    om

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ProfilePage from "./components/ProfilePage";
 import Navbar1 from "./components/Navbar1";
+import Footer1 from "./components/Footer1";
 
 function App() {
   return (
     <>
     <Navbar1/>
     <ProfilePage/>
-      
+      <Footer1/>
     </>
   );
 }
--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -25,7 +25,7 @@ const Navbar1= function (){
   <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
 </svg></div>
 {/* fine search in mobile----------------------------------------------- */}
-    <Container fluid className='d-flex mx-1 justify-content-lg-center justify-content-around  align-items-center sticky-lg-top fixed-bottom pt-1 bg-white' >
+    <Container fluid id='myNav' className='d-flex  justify-content-lg-center justify-content-around  align-items-center sticky-lg-top fixed-bottom pt-1 bg-white' >
         {/* logo + search ---------------------------------------------------------- */}
         <div className='d-none d-lg-flex align-items-center me-5'><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="blue" class="bi bi-linkedin " viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
