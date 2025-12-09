682b71130d0e09cb516003937f8fda639502fb2e
Hammers <gianluca.hammers@gmail.com>
2025-12-09 11:20:33 +0100

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
-import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
-      <Banner />
+      <div></div>
     </>
   );
 }
