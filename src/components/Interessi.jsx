5700c8c27f37ab5ccda1564356c24a7ca0c5c9ca
Filippo-Timo <filtimo2002@gmail.com>
2025-12-10 15:51:28 +0100

    Updates buttons under little components titles

--- src/components/Interessi.jsx
+++ src/components/Interessi.jsx
@@ -7,11 +7,11 @@ import "../assets/ButtonsHover.css";
 const Interessi = () => {
     return (
         <Card className="text-center mb-3">
-            <Card.Body>
+            <Card.Body className="p-0">
                 <Card.Title className="text-start mb-4">
-                    <h3 className="m-0">Interessi</h3>
+                    <h3 className="m-0 ms-3 mt-3">Interessi</h3>
                     <div className="border-bottom border-1 border-secondary">
-                        <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 pe-3 ps-0 border-0 rounded-0">Aziende</Button>
+                        <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 px-3 ms-3 border-0 rounded-0">Aziende</Button>
                         <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 px-3 ms-2 border-0 rounded-0">Gruppi</Button>
                         <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 px-3 ms-2 border-0 rounded-0">Scuole o università</Button>
                     </div>
--- src/components/Referenza.jsx
+++ src/components/Referenza.jsx
@@ -7,11 +7,11 @@ import "../assets/ButtonsHover.css";
 const Referenza = () => {
     return (
         <Card className="text-center mb-3">
-            <Card.Body>
+            <Card.Body className="p-0">
                 <Card.Title className="text-start mb-4">
-                    <h3 className="m-0">Referenze</h3>
+                    <h3 className="m-0 ms-3 mt-3">Referenze</h3>
                     <div className="border-bottom border-1 border-secondary">
-                        <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 pe-3 ps-0 border-0 rounded-0">Ricevute</Button>
+                        <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 px-3 ms-3 border-0 rounded-0">Ricevute</Button>
                         <Button variant="link" className="referenza-buttons text-decoration-none text-dark fs-6 py-2 px-3 ms-2 border-0 rounded-0">Scritte</Button>
                     </div>
                 </Card.Title>
