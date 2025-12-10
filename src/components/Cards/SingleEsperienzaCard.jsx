0ceed49c228da1d5de81e780a4d97791f8f0fb82
Emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-12-10 12:43:59 +0100

    [UPD] Expirence post fetch created, whit modal and modal for  put end delate fetch

--- src/components/Cards/SingleEsperienzaCard.jsx
+++ src/components/Cards/SingleEsperienzaCard.jsx
@@ -1,25 +1,38 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
-const SingleEsperienzaCard = () => {
-    return (
-        <Container className="mb-3">
-            <Row className="d-flex justify-content-center">
-                <Col xs={2} md={1} >
-                    <div>
-                        <img src="https://placedog.net/50/50" alt="doggo"></img>
-                    </div>
-                </Col>
-                <Col xs={10} md={11}>
-                    <div className="text-start">
-                        <p className="fw-bold m-0">Teacher</p>
-                        <p className="m-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint praesentium accusantium omnis </p>
-                        <p className="text-secondary m-0">giu 2019 - Presente - 6 anni e 7 mesi</p>
-                        <p className="text-secondary m-0">Roma, Italia</p>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+const SingleEsperienzaCard = (props) => {
+  return (
+    <Container className="mb-3">
+      <Row className="d-flex justify-content-center">
+        <Col xs={2} md={1}>
+          <div>
+            <img
+              src={props.exp.image || "https://placedog.net/50/50"}
+              alt={props.exp.company || "logo compagnia"}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+        </Col>
 
-export default SingleEsperienzaCard
+        <Col xs={10} md={11}>
+          <div className="text-start">
+            <p className="fw-bold m-0">{props.exp.role}</p>
+            <p className="m-0">
+              <i class="bi bi-building-fill"> </i>
+              {props.exp.company} -{" "}
+              {props.exp.description || "Nessuna descrizione inserita"}
+            </p>
+            <p className="m-0"></p>
+            <p className="text-secondary m-0">
+              {props.exp.startDate} <i class="bi bi-arrow-right-short"></i>
+              {props.exp.endDate || "Attuale"}
+            </p>
+            <p className="text-secondary m-0">{props.exp.area}</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SingleEsperienzaCard;
