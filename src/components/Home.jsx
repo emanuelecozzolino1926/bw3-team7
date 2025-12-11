a15dc53ee476001758129e3eac784deb4cefb664
Mattia Zerbini <zermatty6@gmail.com>
2025-12-11 11:20:16 +0100

    Add creapost

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,18 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
 import LeftCards from './HomeCards/LeftCard';
+import CreatePost from './HomeCards/CreatePost';
 
 
 const Home = function(){
       return(<><Container>
     <Row className='ms-2 primaHome mt-xs-5  mt-sm-5  mt-md-5  mt-lg-5  mt-xl-5'>
         <Col className='col-md-4 col-lg-3 ms-2 '><LeftCards/>
-    </Col><Col className='col-lg-5 col-md-8'></Col><Col className='d-none d-lg-flex col-lg-4 '></Col></Row></Container>
+    </Col><Col className='col-lg-5 col-md-8'>
+    <CreatePost />
+    </Col>
+    <Col className='d-none d-lg-flex col-lg-4 '></Col>
+    </Row></Container>
 </>)
 };
 export default Home
---
+++ src/components/HomeCards/CreatePost.jsx
@@ -0,0 +1,50 @@
+import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+
+function CreatePost() {
+   const [profileData, setProfileData] = useState(null);
+         
+        
+          const apiUrl = "https://striveschool-api.herokuapp.com/api/profile/me";
+          const apiKey =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM3ZmU0NWQzMjJmNTAwMTUxMDc2YzIiLCJpYXQiOjE3NjUyNzcyNTMsImV4cCI6MTc2NjQ4Njg1M30.6vFhBSWn_BZLNsof5SqbWvb4UQfXAP1wchtNpfCrZmI";
+        
+          const getBanner = () => {
+            fetch(apiUrl, {
+              headers: {
+                Authorization: `Bearer ${apiKey}`,
+              },
+            })
+              .then((response) => {
+                if (response.ok) {
+                  console.log(`Collegato al server ${response.status}`);
+                  return response.json();
+                } else {
+                  throw new Error(
+                    `Errore nella connessione al server ${response.status}`
+                  );
+                }
+              })
+              .then((data) => {
+                setProfileData(data);
+              })
+              .catch((error) => {
+                console.log(`Impossibile accedere al server ${error}`);
+              });
+          };
+        
+          useEffect(() => {
+            getBanner();
+          }, []);
+  return (
+    <Card>
+      <Card.Body><img style={{width:'55px',height:'55px'}}
+            src={profileData?.image}
+            alt="pic-profile"
+            className="rounded-circle"
+          /></Card.Body>
+    </Card>
+  );
+}
+
+export default CreatePost;
