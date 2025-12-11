9a90e42fcc721c52b2d64cdb436e3235796d2bc2
LuxS94 <simonetti.lucio@libero.it>
2025-12-11 12:50:38 +0100

    mm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ import LeftCards from './HomePage/HomeCards/LeftCard';
 
 const Home = function () {
     return (<><Container>
-        <Row className='ms-2 primaHome mt-lg-5  '>
-            <Col className='col-md-4 col-lg-3 ms-2 '><LeftCards />
+        <Row className='ms-md-2 primaHome mt-lg-5  '>
+            <Col className='col-md-4 col-lg-3 col-12 ms-md-2 '><LeftCards />
             </Col><Col className='col-lg-5 col-md-8'></Col><Col className='d-none d-lg-flex col-lg-4 '></Col></Row></Container>
     </>)
 };
