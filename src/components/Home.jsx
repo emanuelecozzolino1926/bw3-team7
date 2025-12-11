f565c2823d1fdd3614fe037bfc8d4da619ff50a7
Filippo-Timo <filtimo2002@gmail.com>
2025-12-11 15:12:32 +0100

    Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,26 +6,28 @@ import LeftCards from "./HomePage/HomeCards/LeftCard";
 import CreatePost from "./HomePage/HomeCards/CreatePost";
 import ColonnaDestra from "./ColonnaDestra";
 import FooterHome from "./HomePage/FooterHome";
+import Consigliati from "./HomePage/Consigliati";
 
 const Home = function () {
-  return (
-    <>
-      <Container>
-        <Row className="ms-2 primaHome mt-xs-5  mt-sm-5  mt-md-5  mt-lg-5  mt-xl-5">
-          <Col className="col-md-4 col-lg-3 ms-2 ">
-            <LeftCards />
-          </Col>
-          <Col className="col-lg-5 col-md-8">
-            <CreatePost />
-          </Col>
-          <Col className="d-none d-lg-block col-lg-3 ">
-            <ColonnaDestra title="Today's puzzle" people={1} />
-            <ColonnaDestra title="Add your feed" people={3} />
-            <FooterHome />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Container>
+                <Row className="ms-2 primaHome mt-xs-5  mt-sm-5  mt-md-5  mt-lg-5  mt-xl-5">
+                    <Col className="col-md-4 col-lg-3 ms-2 ">
+                        <LeftCards />
+                    </Col>
+                    <Col className="col-lg-5 col-md-8">
+                        <CreatePost />
+                        <Consigliati people={3} title="Consigli per te" />
+                    </Col>
+                    <Col className="d-none d-lg-block col-lg-3 ">
+                        <ColonnaDestra title="Today's puzzle" people={1} />
+                        <ColonnaDestra title="Add your feed" people={3} />
+                        <FooterHome />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 };
 export default Home;
