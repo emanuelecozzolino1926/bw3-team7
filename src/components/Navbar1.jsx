d1efcaa61d18c084af6d73e7a4f67918cfc9a355
Mattia Zerbini <zermatty6@gmail.com>
2025-12-11 14:29:47 +0100

    Solved problems in console

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -151,7 +151,7 @@ const Navbar1 = function () {
           width="18"
           height="18"
           fill="currentColor"
-          class="bi bi-sort-down ms-3"
+          className="bi bi-sort-down ms-3"
           viewBox="0 0 16 16"
         >
           <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
@@ -161,7 +161,7 @@ const Navbar1 = function () {
           width="18"
           height="18"
           fill="currentColor"
-          class="bi bi-chat-right-dots-fill ms-3"
+          className="bi bi-chat-right-dots-fill ms-3"
           viewBox="0 0 16 16"
         >
           <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
@@ -181,7 +181,7 @@ const Navbar1 = function () {
               width="33"
               height="33"
               fill="blue"
-              class="bi bi-linkedin "
+              className="bi bi-linkedin "
               viewBox="0 0 16 16"
             >
               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
@@ -242,23 +242,22 @@ const Navbar1 = function () {
           <Row className="text-nowrap">
             <Col>
               <Link to={"/"}>
-                <a
+                <span
                   id="icon"
                   className="text-decoration-none text-dark d-flex flex-column align-items-center justify-content-center"
-                  href="#"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
                     fill="currentColor"
-                    class="bi bi-house-door-fill"
+                    className="bi bi-house-door-fill"
                     viewBox="0 0 16 16"
                   >
                     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
                   </svg>
                   <p className="mb-0">Home</p>
-                </a>
+                </span>
               </Link>
             </Col>
             <Col className="ms-1">
@@ -272,7 +271,7 @@ const Navbar1 = function () {
                   width="20"
                   height="20"
                   fill="currentColor"
-                  class="bi bi-people-fill"
+                  className="bi bi-people-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
@@ -291,7 +290,7 @@ const Navbar1 = function () {
                   width="20"
                   height="20"
                   fill="currentColor"
-                  class="bi bi-briefcase-fill"
+                  className="bi bi-briefcase-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5" />
@@ -311,7 +310,7 @@ const Navbar1 = function () {
                   width="20"
                   height="20"
                   fill="currentColor"
-                  class="bi bi-chat-right-dots-fill"
+                  className="bi bi-chat-right-dots-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
@@ -330,7 +329,7 @@ const Navbar1 = function () {
                   width="20"
                   height="20"
                   fill="currentColor"
-                  class="bi bi-bell-fill"
+                  className="bi bi-bell-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
--- src/components/ProfilePage/Cards/SingleCompetenzeCard.jsx
+++ src/components/ProfilePage/Cards/SingleCompetenzeCard.jsx
@@ -13,7 +13,7 @@ const SingleCompentenzeCard = () => {
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                             <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                             </svg>
-                        <p className="m-0 mb-0 d-inline">Foto del profilo di Maksim Sinik Competenze confermate da Maksim Sinik, che ha una grande competenza in questo ambito </p>
+                        <p className="m-0 mb-0 d-inline">Competenze confermate da Pupo (Enzo Ghinazzi), che ha una grande competenza in questo ambito </p>
                         </div>
                         <div className='my-2'>
                          <img src="https://placedog.net/25/25" alt="doggo" className='rounded-4 d-inline me-2'></img>
